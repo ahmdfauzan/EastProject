d24e3456f387d7400c784778818e9f233c924c43
Yischard Meynardi Borean <yizchard0897@gmail.com>
2023-09-29 07:19:48 +0800

    Login with token & import/export excel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,12 +59,12 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-
+                            {{-- 
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            @endif --}}
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
